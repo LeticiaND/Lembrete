d4a498adceee6c46a6c9f1aad99db3c00b7b5817
Leticia <leticiandejesus@icloud.com>
2023-03-03 19:25:31 -0300

    salvando codigo

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const todoList = document.querySelector("#todo-list")
 const editForm = document.querySelector("#edit-form")
 const editInput = document.querySelector("#edit-input")
 const cancelEditBtn = document.querySelector("#cancel-edit-btn")
+
+let oldInputValue
 // Funções
 const saveTodo = (text) =>{
     const todo =  document.createElement("div")
@@ -40,6 +42,16 @@ const toggleFoms = () =>{
     todoForm.classList.toggle("hide")
     todoList.classList.toggle("hide")
 }
+const updateTodo = (text) =>{
+    const todos = document.querySelectorAll(".todo")
+
+    todos.forEach((todo) =>{
+        let todoTitle = todo.querySelector("h3")
+        if(todoTitle.innerText === oldInputValue){
+            todoTitle.innerText = text
+        } 
+    })
+}
 // Eventos
 todoForm.addEventListener("submit", (e ) =>{
     e.preventDefault()
@@ -54,6 +66,11 @@ todoForm.addEventListener("submit", (e ) =>{
 document.addEventListener("click", (e) =>{
     const targetEl = e.target
     const parentEl = targetEl.closest("div") // Pegando a div mais proxima.
+    let todoTitle
+
+    if(parentEl && parentEl.querySelector("h3")){
+        todoTitle = parentEl.querySelector("h3").innerText
+    }
 
     if(targetEl.classList.contains("finish-todo")){
         parentEl.classList.toggle("done")
@@ -63,5 +80,23 @@ document.addEventListener("click", (e) =>{
     }
     if(targetEl.classList.contains("edit-todo")){
         toggleFoms() // esconder um formulario e apresentar outro.
+
+        editInput.value = todoTitle
+        oldInputValue = todoTitle
     }
+})
+cancelEditBtn.addEventListener("click", (e) =>{
+    e.preventDefault
+    toggleFoms()
+})
+
+editForm.addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+    const editInputValue = editInput.value
+
+    if(editInputValue){
+        updateTodo(editInputValue)
+    }
+    toggleFoms()
 })
