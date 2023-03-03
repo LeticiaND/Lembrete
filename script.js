97d6362814d938294613cc5e1a968b31654d3121
Leticia <leticiandejesus@icloud.com>
2023-03-03 17:57:35 -0300

    salvando codigo

--- script.js
+++ script.js
@@ -53,4 +53,10 @@ document.addEventListener("click", (e) =>{
     if(targetEl.classList.contains("finish-todo")){
         parentEl.classList.toggle("done")
     }
+    if(targetEl.classList.contains("remove-todo")){
+        parentEl.remove()
+    }
+    if(targetEl.classList.contains("edit-todo")){
+        console.log("editado")
+    }
 })
